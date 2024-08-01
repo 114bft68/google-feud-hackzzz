af31e19ffbad2e29fcd71a9f9eb7ef440915931c
114bft68 <168547332+114bft68@users.noreply.github.com>
2024-08-01 20:08:48 +0800

    ...

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-let hidden = Array.from(document.querySelectorAll('*')).filter((e) => e.outerHTML === `<div style="background-color: var(--blue); height: 100%; position: absolute; right: 0px; top: 0px; width: 100%;"></div>`;
+let hidden = Array.from(document.querySelectorAll('*')).filter((e) => e.outerHTML === `<div style="background-color: var(--blue); height: 100%; position: absolute; right: 0px; top: 0px; width: 100%;"></div>`);
 let answers = [];
 hidden.map((e) => answers.push(e.previousSibling.innerHTML));
 for (let i = 0; i < hidden.length; i++) {
